b663905e4a1a50ea2961d18240b6df6f69a7f743
Love Bellinder <love.bellinder@gmail.com>
2025-11-24 09:52:32 +0100

    Add project files.

---
+++ Nöjesparken/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nöjesparken
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate(object sender, RoutedEventArgs e)
+        {
+            if (!CheckIfLengthIsOver140cm())
+            {
+                MessageBox.Show("Skriv i din längd i cm. Inga bokstäver!");
+                txtBox.Clear();
+                txtBox.Focus();
+                return;
+            }
+        }
+
+        private bool CheckIfLengthIsOver140cm()
+        {
+            bool isTrue = int.TryParse(txtBox.Text, out int input);
+            if (!isTrue)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+
+
+
+}
