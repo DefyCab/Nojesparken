007448a06d7376e6819950263fb288b7e176dd90
Love Bellinder <love.bellinder@gmail.com>
2025-11-24 12:25:29 +0100

    Formatting!

--- Nöjesparken/MainWindow.xaml.cs
+++ Nöjesparken/MainWindow.xaml.cs
@@ -122,12 +122,12 @@ namespace Nöjesparken
 
             for (int i = 0; i < lengths.Length; i++)
             {
-                if (lengths[i]<=139 && lengths[i]>=130)
+                if (lengths[i] <= 139 && lengths[i] >= 130)
                 {
                     people++;
-                }    
+                }
             }
-            return people; 
+            return people;
         }
     }
 }
